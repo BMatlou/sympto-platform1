917cb06f9a29bdeb74265d022940ff7c416bc342
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 14:05:09 +0200

    feat: expose medication adherence through loaded queue module

--- api/src/modules/notification-queue/notification-queue.module.ts
+++ api/src/modules/notification-queue/notification-queue.module.ts
@@ -1,22 +1,15 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from '../../database/database.module';
-
 import { NotificationQueueController } from './notification-queue.controller';
 import { NotificationQueueService } from './notification-queue.service';
 import { NotificationProcessorService } from '../notification-processor/notification-processor.service';
+import { MedicationAdherenceController } from '../medication-adherence/medication-adherence.controller';
+import { MedicationAdherenceService } from '../medication-adherence/medication-adherence.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [
-    NotificationQueueController,
-  ],
-  providers: [
-    NotificationQueueService,
-    NotificationProcessorService,
-  ],
-  exports: [
-    NotificationQueueService,
-  ],
+  controllers: [NotificationQueueController, MedicationAdherenceController],
+  providers: [NotificationQueueService, NotificationProcessorService, MedicationAdherenceService],
+  exports: [NotificationQueueService, MedicationAdherenceService],
 })
 export class NotificationQueueModule {}
