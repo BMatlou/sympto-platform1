66aa9c7a9b368117293dff4832ec634703213b06
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 08:40:39 +0200

    Connect medication reminders to notification infrastructure

--- api/src/modules/patient-medications/patient-medications.module.ts
+++ api/src/modules/patient-medications/patient-medications.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { NotificationQueueModule } from '../notification-queue/notification-queue.module';
 
 import { PatientMedicationsController } from './patient-medications.controller';
 import { PatientMedicationsService } from './patient-medications.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, NotificationsModule, NotificationQueueModule],
   controllers: [PatientMedicationsController],
   providers: [PatientMedicationsService],
   exports: [PatientMedicationsService],
