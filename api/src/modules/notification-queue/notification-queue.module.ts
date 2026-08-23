b1858a4bda0f0da843849f6324ccf74d59a6bfee
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 14:05:30 +0200

    feat: register adherence goal service

--- api/src/modules/notification-queue/notification-queue.module.ts
+++ api/src/modules/notification-queue/notification-queue.module.ts
@@ -5,11 +5,12 @@ import { NotificationQueueService } from './notification-queue.service';
 import { NotificationProcessorService } from '../notification-processor/notification-processor.service';
 import { MedicationAdherenceController } from '../medication-adherence/medication-adherence.controller';
 import { MedicationAdherenceService } from '../medication-adherence/medication-adherence.service';
+import { MedicationAdherenceGoalService } from '../medication-adherence/medication-adherence-goal.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationQueueController, MedicationAdherenceController],
-  providers: [NotificationQueueService, NotificationProcessorService, MedicationAdherenceService],
-  exports: [NotificationQueueService, MedicationAdherenceService],
+  providers: [NotificationQueueService, NotificationProcessorService, MedicationAdherenceService, MedicationAdherenceGoalService],
+  exports: [NotificationQueueService, MedicationAdherenceService, MedicationAdherenceGoalService],
 })
 export class NotificationQueueModule {}
