1de2353080858d00096d2e15590bd762e1873ebe
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 08:33:18 +0200

    feat(api): register Daily Journal generator

--- api/src/modules/health-home/health-home.module.ts
+++ api/src/modules/health-home/health-home.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { HealthHomeController } from './health-home.controller';
 import { HealthHomeService } from './health-home.service';
+import { DailyJournalService } from './daily-journal.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [HealthHomeController],
-  providers: [HealthHomeService],
-  exports: [HealthHomeService],
+  providers: [HealthHomeService, DailyJournalService],
+  exports: [HealthHomeService, DailyJournalService],
 })
 export class HealthHomeModule {}
