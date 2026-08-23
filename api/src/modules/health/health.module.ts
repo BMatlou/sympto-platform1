5ac6dacbf75b48582bf400155dcfc5b40802973b
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 12:52:49 +0200

    feat(api): register Health Home in health module

--- api/src/modules/health/health.module.ts
+++ api/src/modules/health/health.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../database/database.module';
 import { HealthController } from './health.controller';
+import { HealthHomeController } from './health-home.controller';
+import { HealthHomeService } from './health-home.service';
 
 @Module({
-  controllers: [HealthController]
+  imports: [DatabaseModule],
+  controllers: [HealthController, HealthHomeController],
+  providers: [HealthHomeService],
 })
 export class HealthModule {}
