c1918423a4d28e08f5d752d4a1d5646917fa9ebe
BMatlou <bankies.matlou2@gmail.com>
2026-08-23 22:56:40 +0200

    Remove dashboard quick-access card grid

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -1,13 +1,5 @@
 import HealthHome from "@/components/dashboard/health-home";
-import HealthQuickActions from "@/components/dashboard/health-quick-actions";
 
 export default function DashboardRoute() {
-  return (
-    <>
-      <HealthHome />
-      <div className="mx-auto max-w-7xl px-4 pb-10 sm:px-6 lg:px-8">
-        <HealthQuickActions />
-      </div>
-    </>
-  );
+  return <HealthHome />;
 }
