15d8b163fe53fbb53a15b0c5138023b889cfce28
BMatlou <bankies.matlou2@gmail.com>
2026-08-24 19:07:43 +0200

    feat(profile): wire profile record editing into onboarding module

--- api/src/modules/onboarding/onboarding.module.ts
+++ api/src/modules/onboarding/onboarding.module.ts
@@ -6,6 +6,8 @@ import { HealthHomeModule } from '../health-home/health-home.module';
 import { OnboardingController } from './onboarding.controller';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingRepository } from './onboarding.repository';
+import { ProfileRecordController } from './profile-record.controller';
+import { ProfileRecordService } from './profile-record.service';
 
 @Module({
   imports: [
@@ -14,13 +16,16 @@ import { OnboardingRepository } from './onboarding.repository';
   ],
   controllers: [
     OnboardingController,
+    ProfileRecordController,
   ],
   providers: [
     OnboardingService,
     OnboardingRepository,
+    ProfileRecordService,
   ],
   exports: [
     OnboardingService,
+    ProfileRecordService,
   ],
 })
 export class OnboardingModule {}
