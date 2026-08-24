fccc16a90a36c68ba551c6372c724e096f19974d
BMatlou <bankies.matlou2@gmail.com>
2026-08-24 15:04:09 +0200

    feat: register patient context service

--- api/src/modules/health/health.module.ts
+++ api/src/modules/health/health.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from '../../database/database.module';
 import { HealthController } from './health.controller';
 import { HealthHomeController } from './health-home.controller';
 import { HealthHomeService } from './health-home.service';
+import { PatientContextService } from './patient-context.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [HealthController, HealthHomeController],
-  providers: [HealthHomeService],
+  providers: [HealthHomeService, PatientContextService],
 })
 export class HealthModule {}
