e491a621788936abf674444d2b98e1c2e7beda54
BMatlou <bankies.matlou2@gmail.com>
2026-09-10 07:57:08 +0200

    feat: register unified Smart File consent service

--- api/src/modules/data-access-consents/data-access-consents.module.ts
+++ api/src/modules/data-access-consents/data-access-consents.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from '../../database/database.module';
 
 import { DataAccessConsentsController } from './data-access-consents.controller';
 import { DataAccessConsentsService } from './data-access-consents.service';
+import { SmartFileConsentController } from './smart-file-consent.controller';
+import { SmartFileConsentService } from './smart-file-consent.service';
 
 @Module({
   imports: [
@@ -12,14 +14,17 @@ import { DataAccessConsentsService } from './data-access-consents.service';
 
   controllers: [
     DataAccessConsentsController,
+    SmartFileConsentController,
   ],
 
   providers: [
     DataAccessConsentsService,
+    SmartFileConsentService,
   ],
 
   exports: [
     DataAccessConsentsService,
+    SmartFileConsentService,
   ],
 })
 export class DataAccessConsentsModule {}
