9a2c5395244c145560dcec753ac7eb052d596824
BMatlou <bankies.matlou2@gmail.com>
2026-09-11 23:28:05 +0200

    feat: register health goal intelligence service

--- api/src/modules/health-goals/health-goals.module.ts
+++ api/src/modules/health-goals/health-goals.module.ts
@@ -4,6 +4,7 @@ import { DatabaseModule } from '../../database/database.module';
 
 import { HealthGoalsController } from './health-goals.controller';
 import { HealthGoalsService } from './health-goals.service';
+import { HealthGoalIntelligenceService } from './health-goal-intelligence.service';
 
 @Module({
   imports: [DatabaseModule],
@@ -14,10 +15,12 @@ import { HealthGoalsService } from './health-goals.service';
 
   providers: [
     HealthGoalsService,
+    HealthGoalIntelligenceService,
   ],
 
   exports: [
     HealthGoalsService,
+    HealthGoalIntelligenceService,
   ],
 })
 export class HealthGoalsModule {}
