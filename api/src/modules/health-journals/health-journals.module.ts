8dcb79429451557eaf102c4729a561a02eaf60cb
BMatlou <bankies.matlou2@gmail.com>
2026-09-11 23:28:18 +0200

    feat: connect journals to goal intelligence

--- api/src/modules/health-journals/health-journals.module.ts
+++ api/src/modules/health-journals/health-journals.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 
 import { HealthJournalsController } from './health-journals.controller';
 import { HealthJournalsService } from './health-journals.service';
 import { SymptomIntelligenceService } from './symptom-intelligence.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    HealthGoalsModule,
+  ],
 
   controllers: [
     HealthJournalsController,
