d293400e6513174682cf3be231e9134cd1f5ea6f
BMatlou <bankies.matlou2@gmail.com>
2026-09-11 23:35:18 +0200

    feat: connect medication adherence to health goals

--- api/src/modules/patient-medications/patient-medications.module.ts
+++ api/src/modules/patient-medications/patient-medications.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { NotificationQueueModule } from '../notification-queue/notification-queue.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 
 import { PatientMedicationsController } from './patient-medications.controller';
 import { PatientMedicationsService } from './patient-medications.service';
 
 @Module({
-  imports: [DatabaseModule, NotificationsModule, NotificationQueueModule],
+  imports: [DatabaseModule, NotificationsModule, NotificationQueueModule, HealthGoalsModule],
   controllers: [PatientMedicationsController],
   providers: [PatientMedicationsService],
   exports: [PatientMedicationsService],
