ebb00f3f2635cd5654137e323db7b19395ee9f1c
BMatlou <bankies.matlou2@gmail.com>
2026-09-11 09:43:28 +0200

    feat: register secure patient wearable connector

--- api/src/modules/wearable-devices/wearable-devices.module.ts
+++ api/src/modules/wearable-devices/wearable-devices.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from '../../database/database.module';
 
 import { WearableDevicesController } from './wearable-devices.controller';
 import { WearableDevicesService } from './wearable-devices.service';
+import { PatientWearablesController } from '../patient-wearables/patient-wearables.controller';
+import { PatientWearablesService } from '../patient-wearables/patient-wearables.service';
 
 @Module({
   imports: [
@@ -12,14 +14,17 @@ import { WearableDevicesService } from './wearable-devices.service';
 
   controllers: [
     WearableDevicesController,
+    PatientWearablesController,
   ],
 
   providers: [
     WearableDevicesService,
+    PatientWearablesService,
   ],
 
   exports: [
     WearableDevicesService,
+    PatientWearablesService,
   ],
 })
 export class WearableDevicesModule {}
