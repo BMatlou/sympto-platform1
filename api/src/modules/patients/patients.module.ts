faf28f23fc4b8fdc39c58e869a56598bc0001cda
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 14:50:12 +0200

    fix: expose health goal engine to patient updates

--- api/src/modules/patients/patients.module.ts
+++ api/src/modules/patients/patients.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 import { PatientsRepository } from './patients.repository';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HealthGoalsModule],
 
   controllers: [PatientsController],
 
   providers: [PatientsService, PatientsRepository],
 
   exports: [PatientsService],
-
 })
 export class PatientsModule {}
