0b642c5103666f17759e5458de4d02206bb93325
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 14:51:19 +0200

    fix: expose health goal engine to clinical vitals

--- api/src/modules/clinical-vitals/clinical-vitals.module.ts
+++ api/src/modules/clinical-vitals/clinical-vitals.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from '../../database/database.module';
-
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 import { ClinicalVitalsController } from './clinical-vitals.controller';
 import { ClinicalVitalsService } from './clinical-vitals.service';
 
 @Module({
-  imports: [DatabaseModule],
-
+  imports: [DatabaseModule, HealthGoalsModule],
   controllers: [ClinicalVitalsController],
-
   providers: [ClinicalVitalsService],
-
   exports: [ClinicalVitalsService],
 })
 export class ClinicalVitalsModule {}
