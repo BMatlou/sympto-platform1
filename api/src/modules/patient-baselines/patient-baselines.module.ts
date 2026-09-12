90d7a7e6e36462b07d5e883e1095fe6a3f722cfe
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 14:50:35 +0200

    Sync baseline changes with goals

--- api/src/modules/patient-baselines/patient-baselines.module.ts
+++ api/src/modules/patient-baselines/patient-baselines.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from '../../database/database.module';
-
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 import { PatientBaselinesController } from './patient-baselines.controller';
 import { PatientBaselinesService } from './patient-baselines.service';
 
 @Module({
-  imports: [DatabaseModule],
-
-  controllers: [
-    PatientBaselinesController,
-  ],
-
-  providers: [
-    PatientBaselinesService,
-  ],
-
-  exports: [
-    PatientBaselinesService,
-  ],
+  imports: [DatabaseModule, HealthGoalsModule],
+  controllers: [PatientBaselinesController],
+  providers: [PatientBaselinesService],
+  exports: [PatientBaselinesService],
 })
 export class PatientBaselinesModule {}
