530e6e7195a7f9919a34828f707f344374653797
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 14:51:43 +0200

    fix: expose health goal engine to wearable measurements

--- api/src/modules/device-measurements/device-measurements.module.ts
+++ api/src/modules/device-measurements/device-measurements.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from '../../database/database.module';
-
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 import { DeviceMeasurementsController } from './device-measurements.controller';
 import { DeviceMeasurementsService } from './device-measurements.service';
 
 @Module({
-  imports: [
-    DatabaseModule,
-  ],
-
-  controllers: [
-    DeviceMeasurementsController,
-  ],
-
-  providers: [
-    DeviceMeasurementsService,
-  ],
-
-  exports: [
-    DeviceMeasurementsService,
-  ],
+  imports: [DatabaseModule, HealthGoalsModule],
+  controllers: [DeviceMeasurementsController],
+  providers: [DeviceMeasurementsService],
+  exports: [DeviceMeasurementsService],
 })
 export class DeviceMeasurementsModule {}
