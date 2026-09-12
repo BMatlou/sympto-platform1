6080f37110b43dcf2e1027097691a37cdfaa9785
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 09:14:38 +0200

    feat: register contextual goals engine

--- api/src/modules/health-goals/health-goals.module.ts
+++ api/src/modules/health-goals/health-goals.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
-
 import { HealthGoalsController } from './health-goals.controller';
 import { HealthGoalsService } from './health-goals.service';
+import { GoalsEngineService } from './goals-engine.service';
 
 @Module({
   imports: [DatabaseModule],
-
-  controllers: [
-    HealthGoalsController,
-  ],
-
-  providers: [
-    HealthGoalsService,
-  ],
-
-  exports: [
-    HealthGoalsService,
-  ],
+  controllers: [HealthGoalsController],
+  providers: [HealthGoalsService, GoalsEngineService],
+  exports: [HealthGoalsService, GoalsEngineService],
 })
 export class HealthGoalsModule {}
