ed467368d9d85fde5d1549b4d5fb53e63ea98c2a
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 09:14:44 +0200

    feat: connect journals to goals engine

--- api/src/modules/health-journals/health-journals.module.ts
+++ api/src/modules/health-journals/health-journals.module.ts
@@ -1,23 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 
 import { HealthJournalsController } from './health-journals.controller';
 import { HealthJournalsService } from './health-journals.service';
 
 @Module({
-  imports: [DatabaseModule],
-
-  controllers: [
-    HealthJournalsController,
-  ],
-
-  providers: [
-    HealthJournalsService,
-  ],
-
-  exports: [
-    HealthJournalsService,
-  ],
+  imports: [DatabaseModule, HealthGoalsModule],
+  controllers: [HealthJournalsController],
+  providers: [HealthJournalsService],
+  exports: [HealthJournalsService],
 })
 export class HealthJournalsModule {}
