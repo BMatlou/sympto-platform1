8f213993216bd82cababc07b7d0e85f43af7df57
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 14:32:51 +0200

    connect health home to health goal engine

--- api/src/modules/health-home/health-home.module.ts
+++ api/src/modules/health-home/health-home.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 import { HealthHomeController } from './health-home.controller';
 import { HealthHomeService } from './health-home.service';
 import { DailyJournalService } from './daily-journal.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HealthGoalsModule],
   controllers: [HealthHomeController],
   providers: [HealthHomeService, DailyJournalService],
   exports: [HealthHomeService, DailyJournalService],
