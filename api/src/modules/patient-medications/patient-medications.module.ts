270401c7bea003a7ded9b51d0ecf3323ff60723e
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 09:15:39 +0200

    feat: register goals engine in medications module

--- api/src/modules/patient-medications/patient-medications.module.ts
+++ api/src/modules/patient-medications/patient-medications.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { HealthGoalsModule } from '../health-goals/health-goals.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { NotificationQueueModule } from '../notification-queue/notification-queue.module';
 
@@ -8,7 +9,7 @@ import { PatientMedicationsController } from './patient-medications.controller';
 import { PatientMedicationsService } from './patient-medications.service';
 
 @Module({
-  imports: [DatabaseModule, NotificationsModule, NotificationQueueModule],
+  imports: [DatabaseModule, HealthGoalsModule, NotificationsModule, NotificationQueueModule],
   controllers: [PatientMedicationsController],
   providers: [PatientMedicationsService],
   exports: [PatientMedicationsService],
