81e61fcaf46821513e79d6143cac5740fd794bb4
BMatlou <bankies.matlou2@gmail.com>
2026-09-12 09:48:46 +0200

    update health goals module

--- api/src/modules/health-goals/health-goals.module.ts
+++ api/src/modules/health-goals/health-goals.module.ts
@@ -1,26 +1,14 @@
 import { Module } from '@nestjs/common';
-
 import { DatabaseModule } from '../../database/database.module';
-
 import { HealthGoalsController } from './health-goals.controller';
 import { HealthGoalsService } from './health-goals.service';
 import { HealthGoalIntelligenceService } from './health-goal-intelligence.service';
+import { GoalsEngineService } from './goals-engine.service';
 
 @Module({
   imports: [DatabaseModule],
-
-  controllers: [
-    HealthGoalsController,
-  ],
-
-  providers: [
-    HealthGoalsService,
-    HealthGoalIntelligenceService,
-  ],
-
-  exports: [
-    HealthGoalsService,
-    HealthGoalIntelligenceService,
-  ],
+  controllers: [HealthGoalsController],
+  providers: [HealthGoalsService, HealthGoalIntelligenceService, GoalsEngineService],
+  exports: [HealthGoalsService, HealthGoalIntelligenceService, GoalsEngineService],
 })
 export class HealthGoalsModule {}
