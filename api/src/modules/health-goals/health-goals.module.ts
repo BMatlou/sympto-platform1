fbcb3ce43eb3c33a763d5082b951674b3412e3f5
BMatlou <bankies.matlou2@gmail.com>
2026-09-13 20:48:25 +0200

    Register weekly alcohol scheduler in health goals module

--- api/src/modules/health-goals/health-goals.module.ts
+++ api/src/modules/health-goals/health-goals.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from '../../database/database.module';
 import { HealthGoalsController } from './health-goals.controller';
 import { PatientHealthGoalsController } from './patient-health-goals.controller';
@@ -6,15 +7,17 @@ import { HealthGoalsService } from './health-goals.service';
 import { HealthGoalIntelligenceService } from './health-goal-intelligence.service';
 import { GoalsEngineService } from './goals-engine-v3.service';
 import { GoalsEngineService as CategoryAwareGoalsEngineService } from './goals-engine-v2.service';
+import { AlcoholResetScheduler } from './schedulers/alcohol-reset.scheduler';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ScheduleModule.forRoot()],
   controllers: [HealthGoalsController, PatientHealthGoalsController],
   providers: [
     CategoryAwareGoalsEngineService,
     GoalsEngineService,
     HealthGoalsService,
     HealthGoalIntelligenceService,
+    AlcoholResetScheduler,
   ],
   exports: [
     GoalsEngineService,
