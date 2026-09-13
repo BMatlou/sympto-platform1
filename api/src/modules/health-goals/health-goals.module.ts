790d8241ae746643c72305957f9dc9ed568ffa4b
BMatlou <bankies.matlou2@gmail.com>
2026-09-13 21:19:01 +0200

    revert: remove added scheduler wiring from health goals module

--- api/src/modules/health-goals/health-goals.module.ts
+++ api/src/modules/health-goals/health-goals.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from '../../database/database.module';
 import { HealthGoalsController } from './health-goals.controller';
 import { PatientHealthGoalsController } from './patient-health-goals.controller';
@@ -7,17 +6,15 @@ import { HealthGoalsService } from './health-goals.service';
 import { HealthGoalIntelligenceService } from './health-goal-intelligence.service';
 import { GoalsEngineService } from './goals-engine-v3.service';
 import { GoalsEngineService as CategoryAwareGoalsEngineService } from './goals-engine-v2.service';
-import { AlcoholResetScheduler } from './schedulers/alcohol-reset.scheduler';
 
 @Module({
-  imports: [DatabaseModule, ScheduleModule.forRoot()],
+  imports: [DatabaseModule],
   controllers: [HealthGoalsController, PatientHealthGoalsController],
   providers: [
     CategoryAwareGoalsEngineService,
     GoalsEngineService,
     HealthGoalsService,
     HealthGoalIntelligenceService,
-    AlcoholResetScheduler,
   ],
   exports: [
     GoalsEngineService,
