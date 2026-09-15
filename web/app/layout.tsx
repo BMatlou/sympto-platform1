fdba2b20356f396cf5655f6f0259048147b35d28
BMatlou <bankies.matlou2@gmail.com>
2026-09-15 08:30:58 +0200

    Load dedicated Hydration card presentation styles

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import "./hydration-card.css";
 import { Toaster } from "sonner";
 
 import { cn } from "@/lib/utils";
