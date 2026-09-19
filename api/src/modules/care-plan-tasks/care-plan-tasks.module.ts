b85611be266477bc645314671bb6262d70ebd2e9
BMatlou <bankies.matlou2@gmail.com>
2026-09-19 07:51:41 +0200

    Register patient care-plan task endpoint

--- api/src/modules/care-plan-tasks/care-plan-tasks.module.ts
+++ api/src/modules/care-plan-tasks/care-plan-tasks.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 
 import { CarePlanTasksController } from './care-plan-tasks.controller';
+import { PatientCarePlanTasksController } from './patient-care-plan-tasks.controller';
 import { CarePlanTasksService } from './care-plan-tasks.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CarePlanTasksController],
+  controllers: [CarePlanTasksController, PatientCarePlanTasksController],
   providers: [CarePlanTasksService],
   exports: [CarePlanTasksService],
 })
