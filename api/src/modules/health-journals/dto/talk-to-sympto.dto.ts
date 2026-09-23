1d5151f682d0d18378fca74ab1788c3ca2dc3b30
BMatlou <bankies.matlou2@gmail.com>
2026-09-23 11:58:47 +0200

    hardening(symptoms): bound Talk to Sympto input

--- api/src/modules/health-journals/dto/talk-to-sympto.dto.ts
+++ api/src/modules/health-journals/dto/talk-to-sympto.dto.ts
@@ -1,7 +1,8 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class TalkToSymptoDto {
   @IsString()
   @MinLength(2)
+  @MaxLength(4000)
   message!: string;
 }
