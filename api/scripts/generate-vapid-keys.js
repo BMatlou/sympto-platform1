4d40af71478ec88f5cbb39baaad9824f88e4d1de
BMatlou <bankies.matlou2@gmail.com>
2026-09-26 10:43:58 +0200

    Add push notification support file generate-vapid-keys.js

---
+++ api/scripts/generate-vapid-keys.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+  namedCurve: 'prime256v1',
+});
+
+const jwk = publicKey.export({ format: 'jwk' });
+const privatePem = privateKey.export({ format: 'pem', type: 'pkcs8' });
+const publicKeyBytes = Buffer.concat([
+  Buffer.from([0x04]),
+  Buffer.from(jwk.x, 'base64url'),
+  Buffer.from(jwk.y, 'base64url'),
+]);
+
+console.log('\nPUSH_VAPID_PRIVATE_KEY_BASE64=');
+console.log(Buffer.from(privatePem, 'utf8').toString('base64'));
+console.log('\nPUSH_VAPID_PUBLIC_KEY=');
+console.log(publicKeyBytes.toString('base64url'));
+console.log('\nPUSH_VAPID_SUBJECT=mailto:notifications@example.com');
+console.log('\nSet PUSH_VAPID_SUBJECT to an address you control before deployment.\n');
