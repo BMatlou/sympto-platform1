215fb75d6ef0971a695ca423bd92c2b052130734
BMatlou <bankies.matlou2@gmail.com>
2026-09-26 10:45:15 +0200

    Inject push provider into notification processor

--- api/src/modules/notification-processor/notification-processor.module.ts
+++ api/src/modules/notification-processor/notification-processor.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 import { NotificationProcessorService } from './notification-processor.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, NotificationsModule],
   providers: [NotificationProcessorService],
 })
 export class NotificationProcessorModule {}
