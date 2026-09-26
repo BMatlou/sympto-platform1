43f5c6048e35121172b87384d68e0518e8ff661a
BMatlou <bankies.matlou2@gmail.com>
2026-09-26 10:44:52 +0200

    Register push notification provider

--- api/src/modules/notifications/notifications.module.ts
+++ api/src/modules/notifications/notifications.module.ts
@@ -6,11 +6,12 @@ import { NotificationsController } from './notifications.controller';
 import { PatientNotificationsController } from './patient-notifications.controller';
 import { NotificationPreferencesModule } from '../notification-preferences/notification-preferences.module';
 import { NotificationsService } from './notifications.service';
+import { PushNotificationService } from './push-notification.service';
 
 @Module({
   imports: [DatabaseModule, NotificationPreferencesModule],
   controllers: [NotificationsController, PatientNotificationsController],
-  providers: [NotificationsService],
-  exports: [NotificationsService],
+  providers: [NotificationsService, PushNotificationService],
+  exports: [NotificationsService, PushNotificationService],
 })
 export class NotificationsModule {}
