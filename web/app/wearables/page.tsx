b0a22c5ddec7ffeef7c75b731adc79cfb27ba661
BMatlou <bankies.matlou2@gmail.com>
2026-09-26 13:29:54 +0200

    remove unused wearable provider state

--- web/app/wearables/page.tsx
+++ web/app/wearables/page.tsx
@@ -170,7 +170,6 @@ export default function WearablesPage() {
     }
   };
 
-  const symptoProvider = providers.find((provider) => provider.provider === "SYMPTO_WEARABLE");
   const otherProviders = providers.filter((provider) => provider.provider !== "SYMPTO_WEARABLE");
 
   const symptoProvider = providers.find((provider) => provider.provider === "SYMPTO_WEARABLE");
